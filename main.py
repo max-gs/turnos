57c76f446be13d04fb21c92f1c075cfe89185ec2
max-gs <60117097+max-gs@users.noreply.github.com>
2025-06-03 23:33:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import asyncio
 from playwright.async_api import async_playwright
 import requests
 from dotenv import load_dotenv
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Servidor Flask funcionando correctamente en Render."
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render asigna un puerto al azar, usamos ese
+    app.run(host='0.0.0.0', port=port)  # Es necesario escuchar en 0.0.0.0 para que Render lo detecte
+
 
 # Cargar variables de entorno
 load_dotenv()
